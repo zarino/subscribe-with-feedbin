f1fc09e1eb5f8df932a7b1f9b7cf83018d87b2ba
Zarino Zappia <mail@zarino.co.uk>
2013-11-08 21:42:46 +0000

    more comments

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+  // [request] here is a list of feed objects, sent by inject.js
   if(request.length){
     chrome.pageAction.show(sender.tab.id)
+    // We probably want to save the list to local storage,
+    // so page_action.js can load it when icon is clicked
   }
   sendResponse("Ok, thanks!")
 })
