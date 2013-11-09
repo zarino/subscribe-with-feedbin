5c55e19479bd1196a8396d8d2bd7c696c9ab1959
Zarino Zappia <mail@zarino.co.uk>
2013-11-09 15:47:33 +0000

    page_action.html now checks for username and password, and shows a link to options.html if they aren't set

--- page_action.js
+++ page_action.js
@@ -1,18 +1,61 @@
-console.log('page_action.js: opened')
-chrome.tabs.query({
-    active: true,
-    currentWindow: true
-}, function(tabs) {
-  console.log('page_action.js: tab.id =', tabs[0]['id'])
-  console.log('page_action.js: sending "give me feeds" message')
-  chrome.tabs.sendMessage(tabs[0]['id'], {
-    type: "give me feeds"
-  }, function(feeds){
-    console.log('page_action.js: received feeds', feeds)
+// Loads an email and password (or null and null if
+// no details have been saved) from local storage.
+var loadCredentials = function(){
+  var dfd = $.Deferred()
+  chrome.storage.local.get('credentials', function(result){
+    if('credentials' in result){
+      dfd.resolve(result.credentials.email, result.credentials.password)
+    } else {
+      dfd.resolve(null, null)
+    }
+  })
+  return dfd.promise()
+}
+
+// Ask inject.js for a list of feeds on the current page,
+// and then display them for the user to pick from.
+// If the user hasn't yet provided their Feedbin login details
+// we also show them a link to go set them at options.html.
+var showFeeds = function(feeds){
+  var optionsURL = chrome.extension.getURL('options.html')
+  loadCredentials().done(function(email, password){
+    if(email == null || password == null){
+      if(feeds.length == 1){
+        var message = 'Log into Feedbin to subscribe to this feed'
+      } else {
+        var message = 'Log into Feedbin to subscribe to these feeds'
+      }
+      $('body').prepend('<a id="login" class="btn-success" target="_blank" href="' + optionsURL + '">' + message + '</a>')
+    } else {
+      $('body').prepend('<a id="loggedin" target="_blank" href="' + optionsURL + '">Logged in as ' + email + '</a>')
+    }
     var $list = $('<div>').addClass('list-group')
     $.each(feeds, function(i, feed){
-      $list.append('<a class="list-group-item" href="' + feed.url + '">' + feed.title + '</a>')
+      var $a = $('<a class="list-group-item" href="' + feed.url + '">' + feed.title + '</a>')
+      $a.on('click', function(e){
+        e.preventDefault()
+        subscribeToFeed(feed.url, email, password)
+      })
+      $a.appendTo($list)
     })
     $list.appendTo('body')
   })
+}
+
+var subscribeToFeed = function(feedURL, email, password){
+  if(email == null || password == null){
+    return false
+  }
+  alert('coming soon!')
+}
+
+chrome.tabs.query({
+  active: true,
+  currentWindow: true
+}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0]['id'], {
+    type: "give me feeds"
+  }, showFeeds)
 })
+
+
