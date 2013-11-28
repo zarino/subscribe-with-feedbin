15a02da825beeff2b65d362b9aa878e0a047f543
Zarino Zappia <mail@zarino.co.uk>
2013-11-28 21:26:34 +0000

    click a feed to subscribe, then tick/untick tags to tag/untag the feed

--- page_action.js
+++ page_action.js
@@ -19,7 +19,9 @@ var loadCredentials = function(){
 var showFeeds = function(feeds){
   var optionsURL = chrome.extension.getURL('options.html')
   loadCredentials().done(function(email, password){
-    if(email == null || password == null){
+    window.email = email
+    window.password = password
+    if(window.email == null || window.password == null){
       if(feeds.length == 1){
         var message = 'Log into Feedbin to subscribe to this feed'
       } else {
@@ -27,26 +29,113 @@ var showFeeds = function(feeds){
       }
       $('body').prepend('<a id="login" class="btn-success" target="_blank" href="' + optionsURL + '">' + message + '</a>')
     } else {
-      $('body').prepend('<a id="loggedin" target="_blank" href="' + optionsURL + '">Logged in as ' + email + '</a>')
+      $('body').prepend('<a id="loggedin" target="_blank" href="' + optionsURL + '">Logged in as ' + window.email + '</a>')
+      getTags(window.email, window.password).done(function(tags){
+        _.each(tags, function(tag){
+          $('#tags').append('<label class="tag"><input type="checkbox" value="' + tag + '"> ' + tag + '</label>')
+        })
+      }).fail(reportAJAXError)
     }
-    var $list = $('<div>').addClass('list-group')
     $.each(feeds, function(i, feed){
       var $a = $('<a class="list-group-item" href="' + feed.url + '">' + feed.title + '</a>')
-      $a.on('click', function(e){
-        e.preventDefault()
-        subscribeToFeed(feed.url, email, password)
-      })
-      $a.appendTo($list)
+      $a.on('click', feedClick).appendTo('#feeds')
     })
-    $list.appendTo('body')
   })
 }
 
-var subscribeToFeed = function(feedURL, email, password){
-  if(email == null || password == null){
+var feedClick = function(e){
+  e.preventDefault()
+  if(window.email == null || window.password == null){
+    // No login details! Send them to the options screen.
+    $('#login').trigger('click')
     return false
   }
-  alert('coming soon!')
+  var $a = $(this)
+  var feedURL = $(this).attr('href')
+  subscribeToFeed(feedURL, window.email, window.password).done(function(feedObject, jqXHR){
+    $a.addClass('subscribed solo').siblings().slideUp(100)
+    $('#tags').slideDown(100)
+    $('#tags input').on('change', function(e){
+      var $input = $(this)
+      if($input.is(':checked')){
+        tagFeed(feedObject.feed_id, $(this).val()).fail(reportAJAXError)
+      } else {
+        untagFeed(feedObject.feed_id, $(this).val()).fail(reportAJAXError)
+      }
+    })
+  }).fail(reportAJAXError)
+}
+
+// Subscribes to a given feed URL. Returns a jQuery Deferred promise.
+var subscribeToFeed = function(feedURL, email, password){
+  return $.ajax({
+    type: 'POST',
+    url: 'https://api.feedbin.me/v2/subscriptions.json',
+    contentType: 'application/json; charset=utf-8',
+    username: email,
+    password: password,
+    data: JSON.stringify({ feed_url: feedURL })
+  })
+}
+
+// Get all taggings. Returns a jQuery Deferred promise.
+var getTaggings = function(){
+  return $.ajax({
+    url: 'https://api.feedbin.me/v2/taggings.json',
+    username: window.email,
+    password: window.password
+  })
+}
+
+// Get the given user's tags. Returns a jQuery Deferred promise.
+var getTags = function(){
+  var dfd = $.Deferred()
+  getTaggings().done(function(data){
+    dfd.resolve(_.uniq(_.pluck(data, 'name')).sort())
+  }).fail(dfd.reject)
+  return dfd.promise()
+}
+
+// Create a new tagging. Returns a jQuery Deferred promise.
+var tagFeed = function(feed_id, tag){
+  return $.ajax({
+    type: 'POST',
+    url: 'https://api.feedbin.me/v2/taggings.json',
+    contentType: 'application/json; charset=utf-8',
+    username: window.email,
+    password: window.password,
+    data: JSON.stringify({ feed_id: feed_id, name: tag })
+  })
+}
+
+// First get all taggings, then find the one with the
+// matching `feed_id` and `tag`, and delete it
+var untagFeed = function(feed_id, tag){
+  var dfd = $.Deferred()
+  getTaggings().done(function(taggings){
+    // Find the tagging we want
+    var matching_taggings = _.findWhere(taggings, {
+      feed_id: feed_id,
+      name: tag
+    })
+    if(matching_taggings){
+      $.ajax({
+        type: 'DELETE',
+        url: 'https://api.feedbin.me/v2/taggings/' + matching_taggings.id + '.json',
+        username: window.email,
+        password: window.password
+      }).done(dfd.resolve).fail(dfd.reject)
+    } else {
+      dfd.resolve()
+    }
+  }).fail(dfd.reject)
+  return dfd.promise()
+}
+
+// Useful for reporting the error returned by subscribeToFeed()
+var reportAJAXError = function(jqXHR, textStatus, errorThrown){
+  $('body').append('<p id="ajax-error" class="text-danger text-center" title="' + jqXHR.responseJSON.message.replace(/"/g, '&quot;') + '"><strong>Oh no!</strong> ' + jqXHR.status + ' ' + textStatus + '</p>')
+  console.log(jqXHR, textStatus, errorThrown)
 }
 
 chrome.tabs.query({
@@ -57,5 +146,3 @@ chrome.tabs.query({
     type: "give me feeds"
   }, showFeeds)
 })
-
-
